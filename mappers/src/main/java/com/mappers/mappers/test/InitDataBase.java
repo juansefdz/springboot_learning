58503a074537b5bd182d38fb7a7eb61333adfa47
juansefdz <jsfm81091@gmail.com>
2024-06-20 15:24:31 -0500

    ended proyect mapper

--- mappers/src/main/java/com/mappers/mappers/test/InitDataBase.java
+++ mappers/src/main/java/com/mappers/mappers/test/InitDataBase.java
@@ -24,18 +24,35 @@ public class InitDataBase {
     public CommandLineRunner testProductMapperCommand(){
         return args -> {
             List<Product> products = productRepository.findAll();
-            System.out.println("PRODUCTS");
+
             /* primera manera
+            System.out.println("PRODUCTS");
             products.forEach(product -> System.out.println(product));
             */
             //segunda manera mejor
+            //PRODUCTS
+            System.out.println("PRODUCTS");
             products.forEach(System.out::println);
 
+            /* FORMA BASICA DE REALIZAR ESTO
             System.out.println("GET PRODUCTS");
             List<GetProduct>getProductList = products
                     .stream().map(product -> productMapper.toGetDTO(product))
                     .peek(System.out::println) //hace lo mismo que el foreach pero con un return del getproduct
                     .collect(Collectors.toList());
+            */
+            //SEGUNDA FORMA
+
+            //GET PRODUCTS
+            System.out.println("GET PRODUCTS");
+            List<GetProduct>getProductList = productMapper.toGetProductList(products);
+            getProductList.forEach(System.out::println);
+
+            //MAPPED PRODUCTS
+            System.out.println("MAPPED PRODUCTS");
+            List<Product> mappedProducts = productMapper.toEntityList(getProductList);
+            mappedProducts.forEach(System.out::println);
+
         };
     }
 }
